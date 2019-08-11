bb1e4e843e96840ab491b3e27b557d179907bb8e
pingz <807961446@qq.com>
2019-08-11 22:58:34 +0800

    Update server.c

--- 网络/TCP/server.c
+++ 网络/TCP/server.c
@@ -1,61 +1,66 @@
-/* TCP服务器的设置步骤如下：
-1、通过 socket() 系统调用创建一个套接字；  
-2、使用 bind() 系统调用将所创建的套接字绑定到指定的端口上；  
-3、通过 listen() 将进行端口绑定的套接字进行端口侦听，使客户端能够连接；  
-4、通过 accept() 接受客户端的连接，该函数将会被阻塞，直至客户端连接上来；  
-5、数据收发 read / write；  
-*/
-
 #include <stdio.h>
+#include <sys/types.h>          /* See NOTES */
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>  //定义了大量系统调用需要使用的类型
-#include <sys/socket.h>  //定义了大量套接字所需要的结构体
-#include <netinet/in.h>  //与网络相关的结构体及函数
 
-// 该函数将字符串 msg 输出到 stderr，并退出执行
-void error(char* msg){
-  fprintf(stderr, "%s\n", msg);
-  exit(1);
-}
-int main(int argc , char* argv[]){
-  int sockfd;   //保存服务器套接字的文件描述符
-  int newsockfd;   //保存与客户端通讯套接字的文件描述符
-  int portno;   //保存服务器要绑定的端口号
-  int clilen;   //保存client地址的大小，系统调用时需要使用
-  int n;   //存放函数read、write的返回值
-  char buffer[256];   //服务器将从套接字中读取的字符存入该缓存
-  struct sockaddr_in serv_addr, cli_addr ; //存放服务器、客户端套接字属性
-  if(argc < 2) error("Error : no port provided.\n");
-  sockfd = socket(AF_INET , SOCK_STREAM , 0); //创建TCP套接字
-  if(sockfd < 0) error("Error : fail to open socket");
-  memset(&serv_addr , 0 , sizeof(serv_addr));
-  portno = atoi(argv[1]) ;  //将输入的第二个参数转换为端口号
+#define PORT 10000
 
-  // 配置服务器参数
-  serv_addr.sin_family = AF_INET;
-  serv_addr.sin_port = htons(portno);
-  serv_addr.sin_addr.s_addr = INADDR_ANY;  //本机地址 0
+int main()
+{
+	// 创建socket
+	int sockfd = -1;
+	if((sockfd = socket(AF_INET,SOCK_STREAM,0)) < 0)
+	{
+		printf("create socket failed.\n");
+		return -1;
+	}
 
-  if(bind(sockfd, (const struct sockaddr*)&serv_addr, 
-    sizeof(serv_addr) ) < 0){
-    error("Error : binding");
-  }
-  listen(sockfd, 5);  //进行端口侦听
-  clilen = sizeof(cli_addr);
-  //阻塞等待客户端的连接
-  newsockfd = accept(sockfd, (struct sockaddr*) &cli_addr, &clilen);
-  if(newsockfd < 0){
-    error("Error on accept");
-  }
-  memset(buffer, 0, 256);
-  n = read(newsockfd, buffer, 255);
-  if(n < 0) error("Error reading from socket");
-  printf("client message : %s\n" , buffer);
-  n = write(newsockfd, "I got your message", 18);
-  if(n < 0)error("Error writing to socket");
-  close(newsockfd);
-  close(sockfd);
-  return 0;
+	// 绑定
+	struct  sockaddr_in server;
+	server.sin_family = AF_INET;
+	server.sin_port = htons(PORT);
+	server.sin_addr.s_addr = htonl(INADDR_ANY);
+	if(bind(sockfd, (struct sockaddr*)&server, sizeof(server)) < 0)
+	{
+        	printf("bind error\n");
+        	return(1);
+    	}
+
+	// 监听
+	if(listen(sockfd, 5) < 0)
+	{
+	        printf("listen error\n");
+        	return(1);
+    	}		
+
+
+	// 接受连接
+	int clientfd = -1;
+	struct  sockaddr_in client;
+	socklen_t client_len = sizeof(client);
+	while(1)
+	{
+		if ((clientfd = accept(sockfd, (struct sockaddr*)&client, &client_len)) < 0)
+		{
+			printf("accpet error\n");
+			return(-1);
+
+		}
+		else
+		{
+			// 连接成功，接收（发送）数据
+			char buff[100] = {0};
+			if (recv(clientfd, buff, 100, 0) < 0)
+				printf("recv error\n");
+			else
+				printf("recv buff: %s\n", buff);
+			
+			usleep(1000);
+		}
+	}
+	
+
+	return 0;
 }
